2e5989405c969f5450fe5037267166c226f73e15
arek <areksablewski@gmail.com>
2018-03-11 23:13:07 +0100

    added method to check if is valid

--- src/R6PasswordHint.js
+++ src/R6PasswordHint.js
@@ -6,8 +6,8 @@ class R6PasswordHint {
 
         this.selectedRulesState = this._rulesSelectedByUser(params.rules);
         this.domElements = this._getDomElements();
-        
-        console.log(this.selectedRulesState);
+        this._isValid = false;
+
         this._addEventListener();        
     }
 
@@ -15,7 +15,7 @@ class R6PasswordHint {
         let result = {};
 
         for (let rule in this.selectedRulesState) {
-            result[rule] = this.hintScope.querySelector(`.js-R6PasswordHint-${rule}`)
+            result[rule] = this.hintScope.querySelector(`.js-R6PasswordHint-${rule}`);
         }
         
         return result;
@@ -30,7 +30,7 @@ class R6PasswordHint {
                 state[value] = {
                     "counter": 0,
                     "shouldBe": rules[value]
-                }
+                };
             } else {
                 //customRule - TODO
             }
@@ -75,37 +75,48 @@ class R6PasswordHint {
         const value = this.inputField.value;
         
         if (this.selectedRulesState["smallLetter"]) {
-            this._refreshSmallLetterRuleState(value)
+            this._refreshSmallLetterRuleState(value);
         }
 
         if (this.selectedRulesState["bigLetter"]) {
-            this._refreshBigLetterRuleState(value)
+            this._refreshBigLetterRuleState(value);
         }
 
         if (this.selectedRulesState["digit"]) {
-            this._refreshDigitLetterRuleState(value)
+            this._refreshDigitLetterRuleState(value);
         }
 
         if (this.selectedRulesState["lengthBetween"]) {
-            this._refreshLengthBetweenRuleState(value)
+            this._refreshLengthBetweenRuleState(value);
         }
     }
 
-    _isConditionsFulfilled () {        
+    _isConditionsFulfilled () {     
+        let validRules = 0;
+        const numberOfUsedRules = Object.keys(this.selectedRulesState).length;
+
         for (let rule in this.selectedRulesState) {
             if (rule === "lengthBetween") {
                 if (this.selectedRulesState[rule].counter >= this.selectedRulesState[rule].shouldBe[0] && 
                     this.selectedRulesState[rule].counter <= this.selectedRulesState[rule].shouldBe[1]) {
-                        this._toggleClass(rule, true);
-                    }
-                    else {
-                        this._toggleClass(rule, false);
-                    }
+                    validRules += 1;
+                    this._toggleClass(rule, true);
+                }
+                else {
+                    this._toggleClass(rule, false);
+                }
             } else if (this.selectedRulesState[rule].counter >= this.selectedRulesState[rule].shouldBe) {
+                validRules += 1;
                 this._toggleClass(rule, true);
             } else {
                 this._toggleClass(rule, false);
             }
+        }
+
+        if (validRules === numberOfUsedRules) {
+            this._isValid = true;
+        } else {
+            this._isValid = false;
         }
     }
 
@@ -117,5 +128,9 @@ class R6PasswordHint {
         } else {
             element.classList.remove("R6PasswordHint-ok");
         }
+    }
+
+    isValid () {
+        return this._isValid;
     }
 }
